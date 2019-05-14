7e42580739eeb2378075fde9e81584a684ffa52b
Ayce Ciloglugil <ayceciloglugil@gmail.com>
2019-05-14 16:18:18 +0100

    changes for deploying spring boot war into tomcat server

--- src/main/java/com/realm/RealmApplication.java
+++ src/main/java/com/realm/RealmApplication.java
@@ -2,9 +2,10 @@ package com.realm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RealmApplication {
+public class RealmApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealmApplication.class, args);
